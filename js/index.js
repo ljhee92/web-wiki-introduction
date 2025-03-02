e24a8121aafe43ba10af42069f31e9a3a91e92da
ljhee92 <weilainib7@gmail.com>
2025-03-02 21:40:39 +0900

    feat: 댓글 작성 기능 추가

--- js/index.js
+++ js/index.js
@@ -17,3 +17,34 @@ console.log(
 );
 
 console.log("알맞은 스크립트를 작성하세요");
+
+document.addEventListener("DOMContentLoaded", function () {
+   const commentInput = document.querySelector(".comment-input");
+   const submitButton = document.querySelector(".comment-box button[type='button']");
+   const commentList = document.querySelector(".comment-list");
+
+   submitButton.addEventListener("click", function() {
+      const commentText = commentInput.value.trim();
+      if (commentText === "") {
+          alert("댓글을 입력해주세요.");
+          return;
+      }
+
+      const newComment = document.createElement("li");
+      newComment.innerHTML = `
+        <div class="comment-item">
+            <div class="comment-author">
+                <img src="../images/comment-author-icon.png" alt="사용자 프로필 이미지" />
+                <span>방문자</span>
+            </div>
+            <div class="comment-content">
+                ${commentText}
+            </div>
+       </div>
+      `;
+
+      commentList.prepend(newComment);
+      commentInput.value = "";
+      alert("댓글 등록 완료!");
+   });
+});
